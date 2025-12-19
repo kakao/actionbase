a1d7c60754d0df426df25bcfee059807307b87e6
Dokyung Lee <dokyung301@gmail.com>
2025-12-19 15:52:02 +0900

    Set nil as T

--- cli/internal/client/http_client.go
+++ cli/internal/client/http_client.go
@@ -116,6 +116,7 @@ func call[T any](c *HTTPClient, request *http.Request) *Response[T] {
 		if c.context.IsDebuggingEnabled {
 			slog.Debug(fmt.Sprintf("Failed to parse response: %s\n", err.Error()))
 		}
+		var nil T
 		return NewResponse[T](-1, nil, fmt.Errorf("failed to read response Body: %w", err))
 	}
 
