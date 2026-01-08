86fed59d95d959517083402ad224952d10a26958
Dokyung Lee <dokyung301@gmail.com>
2026-01-09 02:32:03 +0900

    Disable interaction buttons during active tour

--- guides/build-your-social-app/src/components/layout/CliTerminal.tsx
+++ guides/build-your-social-app/src/components/layout/CliTerminal.tsx
@@ -235,7 +235,7 @@ const CliTerminal: React.FC = () => {
                     <div className="command-multiline">{renderCommand(currentCommand, commandHistory.length, false)}</div>
                     <button
                       id={`run-command-btn-${currentCommand.stepIndex}-active`}
-                      className={`run-command-btn`}
+                      className={`run-command-btn driver-active-el`}
                       onClick={(e) => {
                         handleRunCommand(currentCommand, commandHistory.length)
                       }}>
