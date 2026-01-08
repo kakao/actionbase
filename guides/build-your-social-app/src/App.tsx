7b09b469ddd528bf9ccbddaaa79a28199dc7bf73
Dokyung Lee <dokyung301@gmail.com>
2026-01-08 15:18:57 +0900

    Apply toast

--- guides/build-your-social-app/src/App.tsx
+++ guides/build-your-social-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {DriverProvider} from './contexts/DriverContext';
+import {ToastProvider} from './contexts/ToastContext';
 import Layout from "./components/layout/Layout";
 import Feed from "./components/pages/Feed";
 import Followers from "./components/pages/Followers";
@@ -11,22 +12,30 @@ import Profile from "./components/pages/Profile";
 import Search from "./components/pages/Search";
 import "./styles/app.css";
 
+function AppContent() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Feed/>}/>
+        <Route path="/profile/:id" element={<Profile/>}/>
+        <Route path="/search" element={<Search/>}/>
+        <Route path="/post/:id" element={<Post/>}/>
+        <Route path="/followers/:id" element={<Followers/>}/>
+        <Route path="/followings/:id" element={<Followings/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </Layout>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <DriverProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Feed/>}/>
-            <Route path="/profile/:id" element={<Profile/>}/>
-            <Route path="/search" element={<Search/>}/>
-            <Route path="/post/:id" element={<Post/>}/>
-            <Route path="/followers/:id" element={<Followers/>}/>
-            <Route path="/followings/:id" element={<Followings/>}/>
-            <Route path="*" element={<NotFound/>}/>
-          </Routes>
-        </Layout>
-      </DriverProvider>
+      <ToastProvider>
+        <DriverProvider>
+          <AppContent/>
+        </DriverProvider>
+      </ToastProvider>
     </BrowserRouter>
   );
 }
