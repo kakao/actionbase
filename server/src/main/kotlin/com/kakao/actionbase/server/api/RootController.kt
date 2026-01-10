f01167bba280de5706e007376a7d2716a3f29403
jaideepkathiresan <jvk7557@gmail.com>
2026-01-10 10:40:01 +0530

    feat(api): add root (/) endpoint with basic status response (#25)

---
+++ server/src/main/kotlin/com/kakao/actionbase/server/api/RootController.kt
@@ -0,0 +1,31 @@
+package com.kakao.actionbase.server.api
+
+import org.springframework.boot.info.BuildProperties
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import java.time.Clock
+import java.time.Instant
+
+@RestController
+class RootController(
+    @Autowired(required = false) private val buildProperties: BuildProperties?
+) {
+
+    @GetMapping("/")
+    fun root(): Mono<ResponseEntity<Map<String, String>>> {
+        val version = buildProperties?.version ?: "0.1.0"
+        val timestamp = Instant.now(Clock.systemUTC()).toString()
+        
+        val response = mapOf(
+            "status" to "UP",
+            "message" to "Actionbase is running",
+            "version" to version,
+            "timestamp" to timestamp
+        )
+        
+        return Mono.just(ResponseEntity.ok(response))
+    }
+}
