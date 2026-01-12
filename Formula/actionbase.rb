994689b629e681f5aabce937e50f8f63ff82f9e2
Dokyung Lee <dokyung301@gmail.com>
2026-01-12 21:31:48 +0900

    Apply GitHub binary asset naming convention

--- Formula/actionbase.rb
+++ Formula/actionbase.rb
@@ -1,21 +1,21 @@
 class Actionbase < Formula
   desc "actionbase CLI"
   homepage "https://github.com/kakao/actionbase"
-  version "0.0.1"
+  version "1.0.0"
   license "Apache-2.0 license"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kakao/actionbase/releases/download/cli/v#{version}/actionbase_darwin_arm64.tar.gz"
+      url "https://github.com/kakao/actionbase/releases/download/cli/v#{version}/actionbase_#{version}_darwin_arm64.tar.gz"
       sha256 ""
     else
-      url "https://github.com/kakao/actionbase/releases/download/cli/v#{version}/actionbase_darwin_amd64.tar.gz"
+      url "https://github.com/kakao/actionbase/releases/download/cli/v#{version}/actionbase_#{version}_darwin_amd64.tar.gz"
       sha256 ""
     end
   end
 
   on_linux do
-    url "https://github.com/kakao/actionbase/releases/download/cli/v#{version}/actionbase_linux_amd64.tar.gz"
+    url "https://github.com/kakao/actionbase/releases/download/cli/v#{version}/actionbase_#{version}_linux_amd64.tar.gz"
     sha256 ""
   end
 
