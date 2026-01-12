9fe289e361e435cd95a352b3e0010736d6241c06
Dokyung Lee <dokyung301@gmail.com>
2026-01-12 20:11:50 +0900

    Restrict CLI HTTP server to loopback address to prevent external access

--- cli/internal/command/guide.go
+++ cli/internal/command/guide.go
@@ -25,52 +25,44 @@ func NewGuide(runner GuideRunner, client *client.ActionbaseClient) *Guide {
 }
 
 func (g *Guide) Execute(args []string) *model.Response {
-	if len(args) < 1 {
+	if len(args) != 2 {
 		fmt.Printf("Usage: %s\n", g.GetType().GetCommand())
 		return nil
 	}
 
-	if args[0] == "stop" {
-		if err := guides.Stop(); err != nil {
-			fmt.Println("Failed to stop guide server:", err)
-		}
+	if args[1] == "start" {
+		fmt.Printf("Usage: %s\n", g.GetType().GetCommand())
 		return nil
 	}
 
-	switch args[1] {
-	case "start":
-		guideTypeString := args[0]
-		guideType, found := guides.TypeFromString(guideTypeString)
-		if !found {
-			fmt.Printf("Invalid guide '%s': only '%s' are supported\n", guideTypeString, strings.Join(guides.SupportedGuideTypes, ","))
-			return nil
-		}
+	guideTypeString := args[0]
+	guideType, found := guides.TypeFromString(guideTypeString)
+	if !found {
+		fmt.Printf("Invalid guide '%s': only '%s' are supported\n", guideTypeString, strings.Join(guides.SupportedGuideTypes, ","))
+		return nil
+	}
 
-		if ok := guides.Download(guideType.Name); !ok {
-			return nil
-		}
+	if ok := guides.Download(guideType.Name); !ok {
+		return nil
+	}
 
-		cwd, err := os.Getwd()
-		if err != nil {
-			fmt.Println("Failed to get current working directory:", err)
-			return nil
-		}
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Failed to get current working directory:", err)
+		return nil
+	}
 
-		src := fmt.Sprintf("%s/%s-latest.zip", cwd, guideType.Name)
-		dest := fmt.Sprintf("%s", cwd)
+	src := fmt.Sprintf("%s/%s-latest.zip", cwd, guideType.Name)
+	dest := fmt.Sprintf("%s", cwd)
 
-		if err := guides.Unzip(src, dest); err != nil {
-			fmt.Println("Failed to unzip guide:", err)
-			return nil
-		}
+	if err := guides.Unzip(src, dest); err != nil {
+		fmt.Println("Failed to unzip guide:", err)
+		return nil
+	}
 
-		host := g.client.GetHost()
-		if err := guides.Start(cwd, guideType.Name, host, g.runner.GetCurrentPort()); err != nil {
-			fmt.Println("Failed to start guide server:", err)
-		}
-
-	default:
-		fmt.Printf("Usage: %s\n", g.GetType().GetCommand())
+	host := g.client.GetHost()
+	if err := guides.Start(cwd, guideType.Name, host, g.runner.GetCurrentPort()); err != nil {
+		fmt.Println("Failed to start guide server:", err)
 	}
 
 	return nil
