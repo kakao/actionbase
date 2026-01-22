28b970d6a86126560ab65f9c7d024ff7848a5c5a
Dokyung Lee <dokyung301@gmail.com>
2026-01-22 21:00:21 +0900

    Update license and build flags

--- Formula/actionbase.rb
+++ Formula/actionbase.rb
@@ -3,12 +3,15 @@ class Actionbase < Formula
   homepage "https://github.com/kakao/actionbase"
   url "https://github.com/kakao/actionbase/archive/refs/tags/cli/0.0.1.tar.gz"
   sha256 "29dc4cfef2956a2b3bda43d4ad097dfbe8996f83b899ca1a7360e6ac56a14bf4"
-  license "Apache-2.0 license"
+  license "Apache-2.0"
 
   depends_on "go" => :build
 
   def install
-    ldflags = "-X main.Version=#{version}"
+    ldflags = %W[
+      -s -w
+      -X main.Version=#{version}
+    ]
 
     cd "cli/cmd/actionbase" do
       system "go", "build", *std_go_args(ldflags: ldflags)
