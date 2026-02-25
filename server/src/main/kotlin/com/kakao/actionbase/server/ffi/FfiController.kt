b120d85735234142f9ec51ccde98673867651462
Minseok Kim <mskim@apache.org>
2026-02-25 10:28:00 +0900

    revert: restore FfiController to base branch state
    Out-of-scope change (import reordering, expression body) that was
    unrelated to SlateDB integration.

--- server/src/main/kotlin/com/kakao/actionbase/server/ffi/FfiController.kt
+++ server/src/main/kotlin/com/kakao/actionbase/server/ffi/FfiController.kt
@@ -1,16 +1,13 @@
 package com.kakao.actionbase.server.ffi
 
 import com.kakao.actionbase.v2.engine.ffi.MathOps
-
-import java.nio.file.Path
-
+import jakarta.annotation.PreDestroy
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-
-import jakarta.annotation.PreDestroy
+import java.nio.file.Path
 
 @RestController
 @ConditionalOnProperty(name = ["ffi.enabled"], havingValue = "true", matchIfMissing = false)
@@ -23,7 +20,9 @@ class FfiController(
     fun add(
         @RequestParam a: Int,
         @RequestParam b: Int,
-    ): Int = mathOps.add(a, b)
+    ): Int {
+        return mathOps.add(a, b)
+    }
 
     @PreDestroy
     fun cleanup() {
