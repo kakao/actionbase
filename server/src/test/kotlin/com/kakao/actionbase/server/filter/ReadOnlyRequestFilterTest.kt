1086dbe6d5a412860a903984eeec83f16abc70eb
Dokyung Lee <112409928+zipdoki@users.noreply.github.com>
2026-06-15 01:23:53 +0900

    fix: register multi-edges/count endpoint in ReadOnlyRequestFilterTest

--- server/src/test/kotlin/com/kakao/actionbase/server/filter/ReadOnlyRequestFilterTest.kt
+++ server/src/test/kotlin/com/kakao/actionbase/server/filter/ReadOnlyRequestFilterTest.kt
@@ -173,6 +173,7 @@ class ReadOnlyRequestFilterTest {
                 "GET /graph/v3/databases/{database}/tables/{table}/edges/counts",
                 "GET /graph/v3/databases/{database}/tables/{table}/edges/get",
                 "GET /graph/v3/databases/{database}/tables/{table}/edges/scan/{index}",
+                "GET /graph/v3/databases/{database}/tables/{table}/multi-edges/count",
                 "GET /graph/v3/databases/{database}/tables/{table}/multi-edges/ids",
                 "GET /graph/v3/databases/{database}/tables/{table}/vertices/get",
                 "GET /graph/v3/datastore",
